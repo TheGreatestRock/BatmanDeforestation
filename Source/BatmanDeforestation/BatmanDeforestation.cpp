1a58a88a6a3bf37c9e9e8f653390bb44809f528c
TheGreatestRock <vic38100@gmail.com>
2025-10-10 12:47:53 +0200

    init

--- Source/BatmanDeforestation/BatmanDeforestation.cpp
+++
@@ -1,6 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-#include "BatmanDeforestation.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BatmanDeforestation, "BatmanDeforestation" );
