71ca4063dcf3ce29caf795144c959495566e29c5
TheGreatestRock <vic38100@gmail.com>
2025-10-10 12:09:11 +0200

    init

---
+++ Source/BatmanDeforestation/BatmanDeforestation.cpp
@@ -0,0 +1,7 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BatmanDeforestation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BatmanDeforestation, "BatmanDeforestation" );
+ 
